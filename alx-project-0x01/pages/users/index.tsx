71dab248d9064e773204e9a5e4c8122578d28c10
MudMoh <mudesir@gmail.com>
2025-09-27 00:51:18 +0300

    Implement the User Modal

--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -1,23 +1,42 @@
 import { GetStaticProps } from 'next';
-import React from 'react';
-import { UserProps } from '@/interfaces';
+import React, { useState } from 'react';
+import { UserProps, UserData } from '@/interfaces';
 import UserCard from '../../components/common/UserCard';
 import Header from "@/components/layout/Header";
 import Footer from "@/components/layout/Footer";
+import UserModal from '../../components/common/UserModal';
 
 interface UsersIndexProps {
   users: UserProps[];
 }
 
 const Users: React.FC<UsersIndexProps> = ({ users }) => {
+  const [isModalOpen, setModalOpen] = useState(false);
+  const [newUser, setNewUser] = useState<UserData | null>(null);
+
+  const handleAddUser = (user: UserData) => {
+    // Implement logic to add the new user to the list
+    console.log('Adding user:', user);
+    setNewUser(user);
+  };
+
   return (
     <div>
       <Header />
       <h1>Users</h1>
+      <button onClick={() => setModalOpen(true)} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        Add User
+      </button>
       {users.map((user) => (
         <UserCard key={user.id} user={user} />
       ))}
       <Footer />
+      {isModalOpen && (
+        <UserModal
+          onClose={() => setModalOpen(false)}
+          onSubmit={handleAddUser}
+        />
+      )}
     </div>
   );
 };
@@ -33,4 +52,4 @@ export async function getStaticProps() {
   }
 }
 
-export default Users; 
+export default Users;
